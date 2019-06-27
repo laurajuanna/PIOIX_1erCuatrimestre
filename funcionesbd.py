d38ad7106174606d5e510a40a43f1043032e5e3e
Laura Ivanna Juiz <juiz.laurai@gmail.com>
2019-06-27 07:05:52 -0300

    FuncionesBD V2

--- funcionesbd.py
+++ funcionesbd.py
@@ -1,14 +1,15 @@
 from datetime import datetime
-
 import sqlite3
 
 base = sqlite3.connect('puntajes.db')
 c = base.cursor()
 
+
 def ejecutar_query(query):
    c.execute(query)
    base.commit()
    return c.fetchall()
+
 
 def guardarJugadores(cantidad,jugadores):# Guarda los jugadores en la BD segun la cantidad indicada y los nombres de la lista jugadores
     for p in range(0,cantidad):
@@ -17,15 +18,18 @@ def guardarJugadores(cantidad,jugadores):# Guarda los jugadores en la BD segun l
                   (None,nombre))
         base.commit()
 
+
 def fechaActual(): # devuelve la fecha actual.
     dt = datetime.now()    # Fecha y hora actual
     laFecha = ("{}/{}/{}".format(dt.day, dt.month, dt.year))
     return laFecha
 
+
 def horaActual(): #devuelve la hora actual
     dt = datetime.now()    # Fecha y hora actual
     laHora = ("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
     return laHora
+
 
 def nuevaPartida(nombrePartida):# Guarda el nombre de la partida en la BD junto a la fecha y hora actual
     laFecha = fechaActual()  # devuelve un str
@@ -33,6 +37,7 @@ def nuevaPartida(nombrePartida):# Guarda el nombre de la partida en la BD junto 
     c.execute("INSERT INTO PARTIDA VALUES (?, ?, ?, ?)",
               (None,nombrePartida,laFecha,laHora))
     base.commit()
+
 
 def ingresoApuntajes (jugadores,nombrePartida):# Ingresa a los jugadores en la tabla PUNTAJES junto a su respectivo ID de partida y jugador
     for t in range(len(jugadores)):
@@ -45,6 +50,7 @@ def ingresoApuntajes (jugadores,nombrePartida):# Ingresa a los jugadores en la t
                   (None,idPartida,idJugador,None,None,None,None,None,None,None,None,None,None,None,None))
     base.commit()
 
+
 def buscarIdPartida(nombrePartida): # Devuelve el ID de la partida a partir del nombre de partida ingresado
     sentencia = ("SELECT ID_PARTIDA FROM PARTIDA WHERE NOMBRE_PARTIDA = ?;")
     c.execute(sentencia,[nombrePartida])
@@ -52,6 +58,7 @@ def buscarIdPartida(nombrePartida): # Devuelve el ID de la partida a partir del 
     idPartida = (busquedaIdPartida[0][0])
     base.commit()
     return idPartida
+
 
 def buscarIdJugador(nombreJugador): # Devuelve el ID del jugador segun el nombre del jugador en la lista Jugadores
     sentencia = ("SELECT ID_JUGADOR FROM JUGADORES WHERE NOMBRE_JUGADOR = ?;")
@@ -61,6 +68,7 @@ def buscarIdJugador(nombreJugador): # Devuelve el ID del jugador segun el nombre
     base.commit()
     return idJugador
 
+
 def buscarIdPuntaje(idPartida,idJugador): # Devuelve el ID de la tabla puntajes segun el ID de partida y jugador(AMBAS DEBEN SER TRUE)
     sentencia = ("SELECT ID_PUNTAJE FROM PUNTAJES WHERE ID_PARTIDA = ? AND ID_JUGADOR = ?;")
     c.execute(sentencia,[idPartida,idJugador])
@@ -69,6 +77,7 @@ def buscarIdPuntaje(idPartida,idJugador): # Devuelve el ID de la tabla puntajes 
     base.commit()
     return idPuntaje
 
+
 def buscarGeneralaIngresada(idPuntaje):#Busca si la generala esta ingresada
     sentencia = ('SELECT "G" FROM PUNTAJES WHERE ID_PUNTAJE = ?;')
     c.execute(sentencia,[idPuntaje])
@@ -76,11 +85,12 @@ def buscarGeneralaIngresada(idPuntaje):#Busca si la generala esta ingresada
     generalaIng = (busquedaGeneralaIng[0][0])
     base.commit()
     if generalaIng == None:
-        return False
+        return True
     elif generalaIng == 0:
         return False
     elif generalaIng == 50:
         return generalaIng
+
 
 def buscarNumIng(idPuntaje,valor):# Para buscar si un numero ya fue ingresado
     if valor == 1:
@@ -144,11 +154,6 @@ def buscarNumIng(idPuntaje,valor):# Para buscar si un numero ya fue ingresado
         else:
             return False
 
-#def buscaPuntajes(idPuntaje):#Busca puntaje ingresado FALTA PROGRAMAR
- #   sentencia = ('SELECT "4" FROM PUNTAJES WHERE ID_PUNTAJE = ?;')
-  #  c.execute(sentencia, [idPuntaje])
-   # busqueda = c.fetchall()
-    #return busqueda
 
 def buscarCatIng(idPuntaje,categoria): # Para buscar si una cat ya fue ingresada
     if categoria == 'E':
@@ -239,22 +244,117 @@ def guardarPuntuacion(categoria,nroTurno,puntos,idPuntaje):#Anota el Nro de turn
         c.execute(sentencia, [nroTurno, puntos, idPuntaje])
     base.commit()
 
-def buscarPartidaGuardada():
+
+def buscarPartidaGuardada():#Muestra las partidas guardadas, permite elegir cual reanudar y devuelve el ID_PARTIDA asociado a ella
     sentencia = ('SELECT * FROM PARTIDA')
     c.execute(sentencia)
     busqueda = c.fetchall()
-    print(">>> ÉSTAS SON LAS PARTIDAS GUARDADAS")
-    for cant in range(0,len(busqueda)):
-        print("")
-        print("> Nombre de partida:",(busqueda[cant][1]))
-        print("Fecha de inicio de partida:",(busqueda[cant][2]))
-        print("Hora de inicio de partida:",(busqueda[cant][3]))
-    nombrePartida = input("\nIngrese el NOMBRE DE PARTIDA que desea reanudar: ")
-    sentencia2 = ('SELECT ID_PARTIDA FROM PARTIDA WHERE NOMBRE_PARTIDA LIKE ?;')
-    c.execute(sentencia2,["%{}%".format(nombrePartida)])
+    if not busqueda:
+        print("*** ERROR. No hay ninguna partida guardada hasta el momento.")
+        print("Inténtelo nuevamente.\n")
+        return False
+    else:
+        print(">>> ÉSTAS SON LAS PARTIDAS GUARDADAS")
+        for cant in range(0, len(busqueda)):
+            print("")
+            print("> Nombre de partida:", (busqueda[cant][1]))
+            print("Fecha de inicio de partida:", (busqueda[cant][2]))
+            print("Hora de inicio de partida:", (busqueda[cant][3]))
+        nombrePartida = input("\nIngrese el NOMBRE DE PARTIDA que desea reanudar: ")
+        sentencia2 = ('SELECT ID_PARTIDA FROM PARTIDA WHERE NOMBRE_PARTIDA LIKE ?;')
+        c.execute(sentencia2, ["%{}%".format(nombrePartida)])
+        busqueda = c.fetchall()
+        if busqueda:
+            idPartida = (busqueda[0][0])
+            print("Los datos fueron ingresados Correctamente!")
+            print("> Partida",nombrePartida,"reanudada con EXITO!")
+            return idPartida
+        elif ValueError:
+            print("\n*** ERROR. Esa partida no existe. Inténtelo nuevamente.\n")
+            resultado = False
+            return resultado
+
+
+def buscarJugadoresGuardados(idPartida): # Devuelve el ID del jugador segun el nombre del jugador en la lista Jugadores
+    sentencia = ("SELECT ID_JUGADOR FROM PUNTAJES WHERE ID_PARTIDA = ?;")
+    c.execute(sentencia,[idPartida])
     busqueda = c.fetchall()
-    idPartida = (busqueda[0][0])
-    return idPartida
+    idAsociado = []
+    nombres = []
+    for can in range(0,len(busqueda)):
+        idAsociado.append(busqueda[can][0])
+    sentencia2 = ('SELECT NOMBRE_JUGADOR FROM JUGADORES WHERE ID_JUGADOR = ?;')
+    for jug in range(0,len(idAsociado)):
+        c.execute(sentencia2,[idAsociado[jug]])
+        jugador = c.fetchall()
+        nombres.append(jugador[0][0])
+    return nombres
+
+
+def buscarPuntajeGuardado(idPartida):
+    sentencia = ('SELECT "1", "2", "3", "4", "5", "6", "E", "F", "P", "G", "GD" FROM PUNTAJES WHERE ID_PARTIDA = ?;')
+    c.execute(sentencia,[idPartida])
+    busqueda = c.fetchall()
+    return busqueda
+
+
+def formatoTablero(busqueda):
+    forTablero = []
+    for m in range(0,len(busqueda)):
+        orden = []
+        puntaje = busqueda[m]
+        for t in range(0,len(puntaje)):
+            valores = (puntaje[t])
+            if valores == None:
+                valores = ''
+                orden.append(valores)
+            else:
+                orden.append(valores)
+        forTablero.append(orden)
+    return forTablero
+
+
+def buscarTurnosGuardado(idPartida):#define los nros de turnos guardados
+    sentencia = ('SELECT TURNO FROM PUNTAJES WHERE ID_PARTIDA = ?;')
+    c.execute(sentencia,[idPartida])
+    turnos = c.fetchall()
+    turnosGuardados = []
+    for m in range(0,len(turnos)):
+        valor = turnos[m][0]
+        if valor == None:
+            turnosGuardados.append(0)
+        else:
+            turnosGuardados.append(valor)
+    return turnosGuardados
+
+def siguienteJug(turnosGuardados):
+    ordenJug = []
+    #print(ordenJug)
+    turnoMax = max(turnosGuardados)
+    #print(turnoMax)
+    #print("cantidad en turnos guardados",len(turnosGuardados))
+    for m in range (0,len(turnosGuardados)):
+        valor = turnosGuardados[m]
+        if turnoMax == valor:
+            ordenJug.append(0)
+        else:
+            ordenJug.append(m)
+    #print(ordenJug)
+    #print(max(ordenJug))
+    siguiente = (max(ordenJug))
+    return siguiente
+
+def turnoSiguiente(turnosGuardados):#define que nro de turno sigue
+    maximo = (max(turnosGuardados))
+    minimo = (min(turnosGuardados))
+    if maximo == 0:
+        turno = 1
+    elif maximo == minimo:
+        turno = (maximo+1)
+    else:
+        turno = maximo
+    return turno
+
 
 def borrarJugadores(idPartida):
     sentencia = ('SELECT ID_JUGADOR FROM PUNTAJES WHERE ID_PARTIDA = ?;')
@@ -273,35 +373,8 @@ def borrarPartida(idPartida):
         sentencia2 = ('DELETE FROM JUGADORES WHERE ID_JUGADOR = ?;')
         c.execute(sentencia2,[borrarId])
     base.commit()
-    print("Partida borrada.")
+    print("Partida borrada Correctamente!.")
 
-buscarPartidaGuardada()
-
-'''
-idPartida = 2
-sentencia = ('SELECT * FROM PUNTAJES WHERE ID_PARTIDA = ?;')
-c.execute(sentencia,[idPartida])
-aca = c.fetchall()
-print(type(aca[0]))
-print(aca[0])
-cantidad = len(aca)
-print("el contenido de la tupla es:",len(aca[0]))
-
-for i in range(0,cantidad):
-    for m in range(4,15):
-        print('Jugador Nro',(i+1),'Puntaje: ',[m],aca[i][m])
-        print(type(aca[i][m]))
-# -------------- HASTA ACA ES LO DEL SELECT QE PROBE ULTIMO
-
-idPuntaje = 6
-valor = 5
-numIng = buscarNumIng(idPuntaje,valor)
-if numIng == False:
-    while numIng == False:
-        print("Usted ya se ha anotado ese número. Intente nuevamente.")
-        valor = input("Ingrese el valor del dado que se anotará: ")
-        numIng = buscarNumIng(idPuntaje, valor)
-print("anotado!")'''
 
 def cerrarBase():# Cierra la base de datos
     c.close()

414a3bea8896605426210f73ac9b0159699111e9
Laura Ivanna Juiz <juiz.laurai@gmail.com>
2019-07-10 19:45:57 -0300

    Principal VFinal

--- principal.py
+++ principal.py
@@ -1,10 +1,3 @@
 import funGenerales
 
-# -------- LO QUE ME FALTA HACER -----------------
-
-# YA ESTA PROGRAMADO LO DE LA DETECCIÓN DE EMPATES
-# ME FALTARIA EMPROLIJAR TODO Y ORDENAR ALGUNAS COSAS
-
-# ------------------------------------------------
-
 funGenerales.iniciarPrograma()

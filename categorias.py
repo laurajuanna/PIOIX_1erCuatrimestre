cd7159454c3eeca3ecacd83866c93d47cbfed090
Laura Ivanna Juiz <juiz.laurai@gmail.com>
2019-07-12 05:47:36 -0300

    Categorias VFinal

--- categorias.py
+++ categorias.py
@@ -1,6 +1,9 @@
-from collections import Counter
+from collections import Counter # Utilizado para el Poker
 
-#me pide un valor, y recorre cuantas veces esta ese valor dentro de la jugada
+# segun los dados obtenidos y un valor de dado pedido al usuario anteriormente
+# recorre la lista de dados para averiguar cuantas veces está ese dado repetido
+# finalmente imprime una cadena con esa información y retorna los puntos obtenidos
+# al multiplicar el valor del dado x la cantidad de veces que se ha repetido
 def SalidaDeNumero(dados,valor):
   cantidad_de_repeticiones=0
   for i in dados:
@@ -10,8 +13,10 @@ def SalidaDeNumero(dados,valor):
   print("El dado",valor,"salio:",cantidad_de_repeticiones,"veces. Obtuvo",multiplicacion,"puntos.")
   return multiplicacion
 
-#la i toma el primer valor de jugada hasta el ultimo, y pregunta valor por valor la caantidad de veces que esta.
-#si el valor i esta 5 veces dentro de jugada, me retorna true
+# La I toma el primer valor del dado hasta el último
+# Luego pregunta valor por valor la cantidad de veces que está repetido
+# Si el valor i esta 5 veces dentro de la jugada es GENERALA y retorna True
+# De lo contrario retorna False
 def EsGenerala(dados):
    for i in range (0,len(dados)):
       if dados.count(dados[i])==5:
@@ -19,19 +24,21 @@ def EsGenerala(dados):
       else:
           return False
 
-# NUEVA VERSION DEL POKER ---> HAY QUE IMPORTAR UN MÓDULO (from collections import Counter)
+# Para encontrar el poker se debe importar el modulo Counter
 def EsPoker(dados):
    cuenta = Counter(dados) # cuenta cuantas repeticiones hay de cada valor
    cuentaRepeticiones = (cuenta.most_common(1)) # genera una lista mostrando qué valor es el mas repetido y cuántas veces se repitió
    maxRepeticion = cuentaRepeticiones[0][1] # muestra el número de veces que se repitió el valor mas repetido
-   if maxRepeticion == 4:# si el valor mas repetido re repitió cuatro veces devuelve True, sino False.
+   if maxRepeticion == 4:# si el valor mas repetido se repitió cuatro veces devuelve True, sino False.
        return True
    else:
        return False
 
-#la i toma el primer valor de jugada hasta el ultimo, y pregunta valor por valor la caantidad de veces que esta.
-#si el valor i esta 3 veces dentro de jugada, me retorna true
-#si hay otro valor que esta 2 veces me retorna true.
+# La i toma el primer valor de jugada hasta el ultimo,
+# pregunta valor por valor la cantidad de veces que está repetido
+# si el valor i esta 3 veces dentro de la jugada, me retorna True
+# si hay otro valor que está 2 veces me retorna True.
+# Si ambos se cumplen retorna True, de lo contrario retorna False.
 def esFull(dados):
     hay_tres=False
     hay_dos=False
@@ -42,8 +49,8 @@ def esFull(dados):
            hay_dos=True
     return hay_tres and hay_dos
 
-#la funcion establece las dos posibles escaleras que existen dentro del juego.
-#luego ordena la jugada, si es igual a alguna de las dos escaleras retorna true.
+# La función establece las dos posibles escaleras que existen dentro del juego.
+# Luego ordena la jugada, si es igual a alguna de las dos escaleras retorna true.
 def esEscalera(dados):
     esc1=[1,2,3,4,5]
     esc2=[2,3,4,5,6]
